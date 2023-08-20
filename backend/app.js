c93aa27de5167f60c387207e0a233eb152622627
Sumit Shrestha <summitshrestha8@gmail.com>
2023-08-20 13:47:18 -0500

    BookStore

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./routes/book-routes');
+const app = express();
+const cors = require('cors');
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use('/books', router);
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:eHWq14QMSjq301hN@cluster0.yrnqi11.mongodb.net/bookStore?retryWrites=true&w=majority'
+  )
+  .then(() => console.log('CONNECTED TO DATABASE'))
+  .then(() => {
+    app.listen(5004);
+  })
+  .catch((err) => console.log(err));
